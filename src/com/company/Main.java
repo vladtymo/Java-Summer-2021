7af0ce1ed1aeffd3feb2d11535614bdc9ae96eb2
vladtymo <tymo.vlad@gmail.com>
2021-08-10 20:01:31 +0300

    Recursion. Intro to Classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,17 @@ public class Main {
         //Lesson6();
         //Lesson7();
         //Lesson8();
-        Lesson9();
+        //Lesson9();
+        //Lesson10();
+
+        Car myCar = new Car();
+
+        //myCar.currentSpeed = -10; // has no access
+        myCar.Stop();
+        myCar.ShowStatus();
+        myCar.Gas();
+        myCar.ShowStatus();
+        myCar.ShowCurrentSpeed();
     }
 
     public static void Lesson1()
@@ -502,5 +512,131 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (char)(random.nextInt(90 - 65 + 1) + 65);
         }
+    }
+
+    public static void Lesson10()
+    {
+        PrintStars(10);
+        System.out.println();
+        //PrintStarsRecursiv(10);
+
+        final int length = 6;
+        int[][] field = new int[length][length];
+
+        FindPathOnChessboard(field, 2, 2);
+
+        for (int r = 0; r < length; r++) {
+            for (int c = 0; c < length; c++) {
+                System.out.print(field[r][c] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void PrintStars(int num) // 10
+    {
+        for (int i = 0; i < num; i++) {
+            System.out.print("*");
+        }
+    }
+    public static void PrintStarsRecursiv(int num)
+    {
+        if (num <= 0) return;
+
+        System.out.print("*");
+        PrintStarsRecursiv(num - 1);
+    }
+
+    // filed cell: true - was the knight
+    //             false - was not the knight
+    public static boolean FindPathOnChessboard(int[][] field, int y, int x) {
+
+        final int nextStep = field[y][x] + 1;
+        if (nextStep > field.length * field.length)
+            return true;
+
+        final int length = field.length;
+
+        if (y - 2 >= 0 && x + 1 < length && field[y - 2][x + 1] == 0) {
+            // try this cell
+            field[y - 2][x + 1] = nextStep;
+            if (FindPathOnChessboard(field, y - 2, x + 1) == false)
+                field[y - 2][x + 1] = 0;
+            else return true;
+        }
+        if (y - 1 >= 0 && x + 2 < length && field[y - 1][x + 2] == 0) {
+            field[y - 1][x + 2] = nextStep;
+            if (FindPathOnChessboard(field, y - 1, x + 2) == false)
+                field[y - 1][x + 2] = 0;
+            else return true;
+        }
+        if (y + 1 < length && x + 2 < length && field[y + 1][x + 2] == 0) {
+            field[y + 1][x + 2] = nextStep;
+            if (FindPathOnChessboard(field, y + 1, x + 2) == false)
+                field[y + 1][x + 2] = 0;
+            else return true;
+        }
+        if (y + 2 < length && x + 1 < length && field[y + 2][x + 1] == 0) {
+            field[y + 2][x + 1] = nextStep;
+            if (FindPathOnChessboard(field, y + 2, x + 1) == false)
+                field[y + 2][x + 1] = 0;
+            else return true;
+        }
+        //------------------------
+        if (y + 2 < length && x - 1 >= 0 && field[y + 2][x - 1] == 0) {
+            field[y + 2][x - 1] = nextStep;
+            if (FindPathOnChessboard(field, y + 2, x - 1) == false)
+                field[y + 2][x - 1] = 0;
+            else return true;
+
+        }
+        if (y + 1 < length && x - 2 >= 0 && field[y + 1][x - 2] == 0) {
+            field[y + 1][x - 2] = nextStep;
+            if (FindPathOnChessboard(field, y + 1, x - 2) == false)
+                field[y + 1][x - 2] = 0;
+            else return true;
+        }
+        if (y - 1 >= 0 && x - 2 >= 0 && field[y - 1][x - 2] == 0) {
+            field[y - 1][x - 2] = nextStep;
+            if (FindPathOnChessboard(field, y - 1, x - 2) == false)
+                field[y - 1][x - 2] = 0;
+            else return true;
+        }
+        if (y - 2 >= 0 && x - 1 >= 0 && field[y - 2][x - 1] == 0) {
+            field[y - 2][x - 1] = nextStep;
+            if (FindPathOnChessboard(field, y - 2, x - 1) == false)
+                field[y - 2][x - 1] = 0;
+            else return true;
+        }
+
+        return false;
+    }
+}
+
+class Car
+{
+    // properties
+    private float currentSpeed;
+
+    // methods
+    private void Stop()
+    {
+        System.out.println("Press break pedal!");
+        currentSpeed = 0;
+    }
+    void Gas()
+    {
+        System.out.println("Press gas pedal!");
+        currentSpeed += 25;
+    }
+    void ShowCurrentSpeed()
+    {
+        System.out.println("Speed: " + currentSpeed + " km/h");
+    }
+    void ShowStatus()
+    {
+        if (currentSpeed <= 0)
+            System.out.println("Car was stopped!");
+        else
+            System.out.println("Car is driving!");
     }
 }
